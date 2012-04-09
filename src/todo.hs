07c100d6ae01d87838534f674233ea8cd9cb94c9
Chris Taylor <crntaylor@gmail.com>
2012-04-09 23:59:37 +0100

    Refactoring

--- src/todo.hs
+++ src/todo.hs
@@ -14,29 +14,26 @@ numberTasks :: [String] -> [String]
 numberTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..]
 
 dispatch :: String -> [String] -> IO ()
-dispatch "add"    = addTask
-dispatch "show"   = showTask
-dispatch "delete" = deleteTask
+dispatch "add"    = add
+dispatch "view"   = view
+dispatch "remove" = remove
 dispatch arg      = doNotRecognize arg
 
 doNotRecognize :: String -> t -> IO ()
 doNotRecognize arg _ = putStrLn ("Did not recognize command: " ++ arg)
 
-addTask :: [String] -> IO ()
-addTask (filePath:tasks) = appendFile filePath (unlines tasks)
+add :: [String] -> IO ()
+add (filePath:tasks) = appendFile filePath (unlines tasks)
 
-showTask :: [String] -> IO ()
-showTask [filePath] = do
+view :: [String] -> IO ()
+view [filePath] = do
     tasks <- getTasks filePath
     mapM_ putStrLn (numberTasks tasks)
-showTask _ = putStrLn "Exactly one argument required with command SHOW"
+view _ = putStrLn "Exactly one argument required with command SHOW"
 
-deleteTask :: [String] -> IO ()
-deleteTask [filePath] = do
+remove :: [String] -> IO ()
+remove [filePath,numberStr] = do
     tasks <- getTasks filePath
-    mapM_ putStrLn (numberTasks tasks)
-    putStrLn "Which one would you like to delete?"
-    numberStr <- getLine
     let newTasks = delete (tasks !! read numberStr) tasks
     bracketOnError (openTempFile "." "temp")
         (\(tempPath, tempHandle) -> do
@@ -47,7 +44,7 @@ deleteTask [filePath] = do
             hClose tempHandle
             removeFile filePath
             renameFile tempPath filePath)
-deleteTask _ = putStrLn "Exactly one argument required with command DELETE"
+remove _ = putStrLn "Exactly two argument required with command DELETE"
 
 main :: IO ()
 main = do
