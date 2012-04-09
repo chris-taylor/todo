008074ce9c4c5ad31e986ad23808883e56186005
Chris Taylor <crntaylor@gmail.com>
2012-04-09 22:10:08 +0100

    Add item

--- src/todo.hs
+++ src/todo.hs
@@ -1,11 +1,17 @@
 import System.Environment
+import System.IO
+
+filePath :: FilePath
+filePath = "../data/todo.txt"
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = addItem
 dispatch "delete" = deleteItem
 
 addItem :: [String] -> IO ()
-addItem = undefined
+addItem tasks = do
+    handle <- openFile filePath AppendMode
+    mapM_ (hPutStrLn handle) tasks
 
 deleteItem :: [String] -> IO ()
 deleteItem = undefined
